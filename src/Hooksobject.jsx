7b8ea32c90e9c327c1903e28a8428592bd3dff74
md famid raza <mdfamid786@gmail.com>
2022-04-09 23:01:39 +0530

    first commit

---
+++ src/Hooksobject.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+export default function Hooksobject(){
+const [a,b]=useState("click me") 
+  let c="#fffff"
+let change=()=>{
+  b("famid raza")
+  b(c)
+}
+  
+    return(
+        <>
+        <button onClick={change}>{a}</button>
+      </>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import  ReactDOM  from "react-dom";
+import "./App.css";
+import App from "./App"
+import "./App.css"
+import Hooksobject from "./Hooksobject"
+
+
+
+ReactDOM.render(
+  <>
+  <App/>
+  <Hooksobject/>
+  </>   
+  ,document.getElementById("root")
+  );
+ 
+ 
+//render takes 2 parameter(what you want show on window,where you have to show )
+ 
+
+
